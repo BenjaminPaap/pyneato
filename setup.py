f77edc63ae7fb6a51cb485a492099cc3feaae233
Benjamin Paap <benjamin.paap@gmail.com>
2023-04-24 10:31:06 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pyneato",
-    version="0.0.7",
+    version="0.0.8",
     description="Python package for controlling Neato pyneato Connected vacuum robot",
     long_description=long_description,
     long_description_content_type="text/markdown",
